1d09c8fc8f15e161ba284bcdcc4adb12fd764240
B1bbot <artem.bolshakov.kaz@gmail.com>
2022-09-19 21:02:53 +0300

    solve 2 tasks

---
+++ Школьный этап/1 вариант/1 var/1 var/Task1.cs
@@ -0,0 +1,10 @@
+﻿class Task1
+{
+    static void Main(string[] args)
+    {
+        int k = int.Parse(Console.ReadLine());
+        int n = int.Parse(Console.ReadLine());
+        int lenghtToNextStop = ((n / k) * k + k) - n;
+        Console.WriteLine(Math.Min(n % k, lenghtToNextStop));
+    }
+}
---
+++ Школьный этап/1 вариант/1 var/Task2/Task2.cs
@@ -0,0 +1,40 @@
+﻿class Task2
+{
+    static void Main(string[] args)
+    {
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+
+        int moreABoxes = 0, moreBBoxes = 0;
+        while (A > 0 && B > 0)
+        {
+            sort();
+        }
+
+        if (A == 0 && B == 0)
+        {
+            showInfo();
+        }
+        else
+        {
+            Console.WriteLine(-1);
+        }
+
+        void showInfo() => Console.WriteLine(string.Format("{0} {1}", moreABoxes, moreBBoxes));
+        void sort()
+        {
+            if (A > B)
+            {
+                moreABoxes++;
+                A -= 2;
+                B--;
+            }
+            else
+            {
+                moreBBoxes++;
+                B -= 2;
+                A--;
+            }
+        }
+    }
+}
