eb2b68e01b4f68b5c1d61e854e75c265727fa4c1
B1bbot <artem.bolshakov.kaz@gmail.com>
2022-10-11 21:02:06 +0300

    add files and solve task1

---
+++ Школьный этап/Сириус 1/Sirius1gr/Sirius1gr/Task1.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int holidays = int.Parse(Console.ReadLine());
+        int day = int.Parse(Console.ReadLine());
+
+        while (day != 1 && day != 7)
+        {
+            holidays--;
+            day++;
+        }
+
+        if (holidays > 0)
+        {
+            Console.WriteLine(holidays / 7);
+        }
+        else
+        {
+            Console.WriteLine(0);
+        }
+    }
+}
