883ae13e6f32cfccb9a0972788cbcf696a7ca27d
B1bbot <artem.bolshakov.kaz@gmail.com>
2022-10-18 12:38:21 +0300

    solve task3

--- Школьный этап/Сириус 1/Sirius1gr/Task3/Task3.cs
+++ Школьный этап/Сириус 1/Sirius1gr/Task3/Task3.cs
@@ -2,6 +2,33 @@
 {
     static void Main(string[] args)
     {
+        int n = int.Parse(Console.ReadLine());
+        List<int> bars = new List<int>();
 
+        for(int i = 0; i < n; i++)
+        {
+            bars.Add(int.Parse(Console.ReadLine()));
+        }
+
+        int equalSides = 0, triangels = 0;
+        int currentSize = 0, previousSize = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            currentSize = bars[i];
+            if (currentSize > previousSize)
+            {
+                equalSides = 0;
+            }
+            equalSides++;
+            if (equalSides == 3)
+            {
+                equalSides = 0;
+                triangels++;
+            }
+            previousSize = currentSize;
+        }
+
+        Console.WriteLine(triangels);
     }
 }
