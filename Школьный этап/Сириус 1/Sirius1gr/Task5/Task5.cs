8ad28ef9e61d7068af210f6aebb8c850d10b229b
B1bbot <artem.bolshakov.kaz@gmail.com>
2022-10-18 15:12:30 +0300

    Update Task5.cs

--- Школьный этап/Сириус 1/Sirius1gr/Task5/Task5.cs
+++ Школьный этап/Сириус 1/Sirius1gr/Task5/Task5.cs
@@ -21,28 +21,27 @@
             if (!(n % 2 == 0 || n % 3 == 0 || n % 5 == 0))
                 return false;
 
-            long border = (long)(Math.Round(n * 0.5 + 1));
-            for (long i = 6; i <= border; i++)
+            while(n != 1)
             {
-                if (isSimple(i))
+                if (n % 2 == 0)
                 {
-                    if (n % i == 0)
-                        return false;
+                    n /= 2;
+                }
+                else if (n % 5 == 0)
+                {
+                    n /= 5;
+                }
+                else if (n % 3 == 0)
+                {
+                    n /= 3;
+                }
+                else
+                {
+                    return false;
                 }
             }
 
             return true;
-        }
-        
-        bool isSimple(long n)
-        {
-            long border = (long)(Math.Round(n * 0.5 + 1));
-            for (long i = 2; i < border; i++)
-            {
-                if (n % i == 0)
-                    return false;
-            }
-            return true;
-        }
+        } 
     }
 }
