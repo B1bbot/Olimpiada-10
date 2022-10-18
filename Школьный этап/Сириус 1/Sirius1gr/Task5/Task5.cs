c43ebaa2e105bda826d190d798e80453615cfce0
B1bbot <artem.bolshakov.kaz@gmail.com>
2022-10-18 12:38:43 +0300

    solve task5

--- Школьный этап/Сириус 1/Sirius1gr/Task5/Task5.cs
+++ Школьный этап/Сириус 1/Sirius1gr/Task5/Task5.cs
@@ -2,6 +2,47 @@
 {
     static void Main(string[] args)
     {
+        long number = long.Parse(Console.ReadLine());
 
+        long maxNumber = 0;
+        for (long n = number; n >= 2; n--)
+        {
+            if (isInteresting(n))
+            {
+                maxNumber = n;
+                break;
+            }
+        }
+
+        Console.WriteLine(maxNumber);
+
+        bool isInteresting(long n)
+        {
+            if (!(n % 2 == 0 || n % 3 == 0 || n % 5 == 0))
+                return false;
+
+            long border = (long)(Math.Round(n * 0.5 + 1));
+            for (long i = 6; i <= border; i++)
+            {
+                if (isSimple(i))
+                {
+                    if (n % i == 0)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+        
+        bool isSimple(long n)
+        {
+            long border = (long)(Math.Round(n * 0.5 + 1));
+            for (long i = 2; i < border; i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
     }
 }
